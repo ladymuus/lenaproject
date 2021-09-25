bd2ed8b17e612ba699a4b88a255bdc3352009145
ladymuus <alexhelen.1@list.ru>
2021-09-25 13:46:20 +0900

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className = 'header'>
+    <img src='https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Content=() => {
+    return (
+<div className='content'>
+<div><img src='https://static.wikia.nocookie.net/6635af79-b530-4d24-8fd6-410aa60be0a9'></img>
+</div>
+<div><img src='https://www.spletnik.ru/img/__post/60/60cf0ac41d8c3404a26fb544a0887c1d_994.jpg'></img>
+</div>
+<div>My post</div>
+<div>New post</div>
+<div>post 1</div>
+<div>post 2</div>
+</div>);
+}
+
+export default Content;
