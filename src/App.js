c3fe7a1c6d0c548ed4c5a5956d7e3a6ccb2eca3e
ladymuus <alexhelen.1@list.ru>
2021-09-27 19:23:05 +0900

    19lesson

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
-const App = () => {
+const App = (props) => {
   return (<div className='app-wrapper'>
     <Header />
     <Nav />
-    <Profile />
-  </div>
+    {/* <Profile /> */}
+    <div className='app-wrapper-content'><Dialogs /></div>
+    </div>
   );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Content=() => {
     return (
-<div className={prof.content}>
+<div>
 <div><img src='https://static.wikia.nocookie.net/6635af79-b530-4d24-8fd6-410aa60be0a9'></img>
 </div>
 {/* <div className={prof.Content}><img src='https://www.spletnik.ru/img/__post/60/60cf0ac41d8c3404a26fb544a0887c1d_994.jpg'></img>
