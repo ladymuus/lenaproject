ab30f91d5a30ea9a1dcb15087f6b1c7473990eaa
ladymuus <alexhelen.1@list.ru>
2021-09-28 14:18:37 +0900

    dobavleny news

---
+++ src/components/News/News.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import n from './News.module.css';
+
+const Newss=() => {
+    return (
+<div>
+<div><img src='https://www.spletnik.ru/img/__post/60/60cf0ac41d8c3404a26fb544a0887c1d_994.jpg'></img>
+</div>
+</div>
+);
+}
+
+export default Newss;
