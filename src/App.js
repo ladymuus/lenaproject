e98c007c460480dd00f01f0c4a604bb13eb47b0d
ladymuus <alexhelen.1@list.ru>
2021-09-28 14:01:39 +0900

    browserweb

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = (props) => {
-  return (<div className='app-wrapper'>
+  return (<BrowserRouter><div className='app-wrapper'>
     <Header />
     <Nav />
-    {/* <Profile /> */}
-    <div className='app-wrapper-content'><Dialogs /></div>
+    <div className='app-wrapper-content'>
+      <Route path='/dialogs' component={Dialogs} />
+      <Route path='/profile' component={Profile}/>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -5,9 +5,9 @@ import n from './Nav.module.css';
 const Nav = () => {
     return(<nav className={n.nav}>
       <div className={n.item}>
-          <a>Profile</a></div>
+          <a href="/profile">Profile</a></div>
       <div className={n.item}>
-        <a>Messages</a></div>
+        <a href="/dialogs">Messages</a></div>
       <div className={n.item}>
         <a>News</a></div>
       <div className={n.item}>
