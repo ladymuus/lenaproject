78b77b8210848514ab528bf48246c90c07604f7a
ladymuus <alexhelen.1@list.ru>
2021-10-05 21:20:08 +0900

    29 state-baza dannyh

---
+++ src/redux/state.js
@@ -0,0 +1,21 @@
+  let state={
+    profilePage:{
+      posts:[
+      {id: 1, message: 'Hi, how are you?', likesCount: ' +15'},
+      {id: 2, message: 'So-so good!))', likesCount: ' +27'}
+    ]
+    },
+    dialogsPage:{
+      messages:[
+      {id: 1, message: 'Heeeeeeyy))'},
+      {id: 2, message: 'What are you doing?'},
+      {id: 3, message: 'See you))'}
+    ],
+    dialogs:[
+      {id: 1, name: 'Max'},
+      {id: 2, name: 'Lena'},
+      {id: 3, name: 'Mika'}
+    ]
+  }
+}
+  export default state;
