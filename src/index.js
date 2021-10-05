dc32833ae64a289dc0fc45c517a263b9c4861537
ladymuus <alexhelen.1@list.ru>
2021-10-05 21:14:08 +0900

    29 state sdelali, так и не поняла зачем сделали

--- src/index.js
+++ src/index.js
@@ -3,24 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state';
 
-let posts=[
-  {id: 1, message: 'Hi, how are you?', likesCount: ' +15'},
-  {id: 2, message: 'So-so good!))', likesCount: ' +27'}
-]
-let dialogs=[
-  {id: 1, name: 'Max'},
-  {id: 2, name: 'Lena'},
-  {id: 3, name: 'Mika'}
-]
-let messages=[
-  {id: 1, message: 'Heeeeeeyy))'},
-  {id: 2, message: 'What are you doing?'},
-  {id: 3, message: 'See you))'}
-]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
