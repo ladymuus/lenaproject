4f53b682f8bb356604252f81140d9de43743e2f6
ladymuus <alexhelen.1@list.ru>
2021-10-18 23:27:46 +0900

    37store,state,bind рефакторинг
    следи за скобками!!!!! в store засунули state, упаковали в один объект, как хранилище типо, т.к. было разбросано

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,8 +1,5 @@
-let rerenderEntireTree = () =>{
-  console.log('state changed');
-}
-
-  let state={
+let store = {
+  _state:{
     profilePage:{
       posts:[
       {id: 1, message: 'Hi, how are you?', likesCount: ' +15'},
@@ -29,28 +26,33 @@ let rerenderEntireTree = () =>{
       {id: 2, name: 'Lena'}
     ]
   }
-}
-
-window.state = state;
-
-export const addPost = () => {
+},
+getState (){
+  return this._state;
+},
+_callSubscriber (){
+  console.log('state changed');
+},
+addPost (){
   let newPost = {
     id:5,
-    message: state.profilePage.newPostText,
+    message: this._state.profilePage.newPostText,
     likesCount: 0
   };
-  state.profilePage.posts.push (newPost);
-  state.profilePage.newPostText='';
-  rerenderEntireTree (state)
+  this._state.profilePage.posts.push (newPost);
+  this._state.profilePage.newPostText='';
+  this._callSubscriber (this._state)
+},
+updateNewPostText (newText){
+  this._state.profilePage.newPostText = newText;
+  this._callSubscriber (this._state)
+},
+subscribe (observer){
+  this._callSubscriber = observer;//обсервер, паттерн
 }
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree (state)
-}
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;//обсервер, паттерн
 }
 
-  export default state;
+  export default store;
+ 
 
   //store - OOP
