8958f12eaba3f792a1c3f6c15f8811f404b39b38
ladymuus <alexhelen.1@list.ru>
2021-10-19 23:33:58 +0900

    39 action creator and action type
    отдали обьекты в бизнес, т.е. больше не создаем в UI

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT='UP-DATE-NEW-POST-TEXT';
+
 let store = {
   _state:{
     profilePage:{
@@ -52,7 +55,8 @@ if(action.type==='ADD-POST'){
 }
 }
 }
-
+export const addPostActionCreator =()=>({type: ADD_POST})
+export const updateNewPostTextActionCreator =(text)=>({type: UPDATE_NEW_POST_TEXT, newText: text})
   export default store;
  window.store=store;
 
